833cd8aa9693947b3bb53d9bfb860d4b12200ffe
FONG JIA <fong_jia_yuan@connect.ite.edu.sg>
2022-01-28 17:36:01 +0800

    Added Base Code PHP

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>The Hygge</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- <link rel='stylesheet' href='header.css'> -->
+</head>
+<body>
+    <nav>
+        <a href="#" class="home">The Hygge</a>
+        <div class="nav-block">
+            <ul>
+                <li>
+                    <a href="#" class="home">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="about">About</a>
+                </li>
+                <li>
+                    <a href="#" class="products">Products</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require 'header.php'?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="grid-layout">
+            
+        </div>
+    </body>
+</html>
+<?php require 'footer.php'?>
