7286ad14ce31300cc3a67615710ee9b59d5eb78b
FONG JIA <fong_jia_yuan@connect.ite.edu.sg>
2022-01-31 16:56:54 +0800

    Revamp Nav code

--- header.php
+++ header.php
@@ -5,24 +5,23 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>The Hygge</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <!-- <link rel='stylesheet' href='header.css'> -->
+    <link rel='stylesheet' type="text/css" href='CSS/header.css'>
 </head>
 <body>
-    <nav>
-        <a href="#" class="home">The Hygge</a>
-        <div class="nav-block">
+    <header>
+        <!-- <img src="" alt="" class="logo"> -->
+        <input type="checkbox" id="tog" class="tog">
+        <nav>
             <ul>
-                <li>
-                    <a href="#" class="home">Home</a>
-                </li>
-                <li>
-                    <a href="#" class="about">About</a>
-                </li>
-                <li>
-                    <a href="#" class="products">Products</a>
-                </li>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">REWARD</a></li>
             </ul>
-        </div>
-    </nav>
+        </nav>
+        <label for="tog" class="tog-label">
+            <span></span>
+        </label>
+    </header>
 </body>
 </html>
