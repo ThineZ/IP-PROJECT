c7a7f06bbad25d70d9bb907d647fb8f8154ae0b7
FONG JIA <fong_jia_yuan@connect.ite.edu.sg>
2022-02-03 14:26:26 +0800

    add and fix carousell

--- index.php
+++ index.php
@@ -1,34 +1,44 @@
 <?php require 'header.php'?>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel='stylesheet' type="text/css" href='CSS/index.css'>
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' type="text/css" href='CSS/index.css'>
+    <link rel='stylesheet' type="text/css" href='CSS/flickity.css'>
+</head>
+
+<body>
+    <div class="grid-layout">
         <div class="grid-layout">
-            <div class="grid-layout">
             <p>GRID</p>
-                <div class="carousell">
-                    <p>Carousell</p>
+            <div class="carousell" data-flickity='{ "wrapAround": true }'>
+                <div class="carousel-cell-one">
+                    <img src="img/1.jpg" alt="1" 
+                    width="100px" height="100px">
                 </div>
-                <div class="intro">
-                    <p>Intro</p>
-                </div>
-                <div class="layout-one">
-                    <p>ONE</p>
-                </div>
-                <div class="layout-two">
-                    <p>TWO</p>
-                </div>
-                <div class="layout-three">
-                    <p>THREE</p>
-                </div>
-                <div class="layout-four">
-                    <p>FOUR</p>
-                </div>
+                <div class="carousel-cell-two"></div>
+                <div class="carousel-cell-three"></div>
+            </div>
+            <div class="intro">
+                <p>Intro</p>
+            </div>
+            <div class="layout-one">
+                <p>ONE</p>
+            </div>
+            <div class="layout-two">
+                <p>TWO</p>
+            </div>
+            <div class="layout-three">
+                <p>THREE</p>
+            </div>
+            <div class="layout-four">
+                <p>FOUR</p>
             </div>
         </div>
-    </body>
+    </div>
+    <script src="JS\flickity.pkgd.js" defer></script>
+</body>
+
 </html>
 <?php require 'footer.php'?>
