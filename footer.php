d99c575408fbb083155f0711a72422691e834aca
FONG JIA <fong_jia_yuan@connect.ite.edu.sg>
2022-02-10 21:53:37 +0800

    foter complete

--- footer.php
+++ footer.php
@@ -1,21 +1,35 @@
 <html>
 
 <head>
-    <link rel='stylesheet' type='text/css' href='footer.css'>
+    <link rel='stylesheet' type='text/css' href='CSS/footer.css'>
 </head>
 
 <body>
     <footer>
-        <div class="end">
-            <img src="CSS/img/logo.png" alt="logo" width="60px" height="60px">
-        </div>
-        <div>
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">ABOUT</a></li>
-                <li><a href="#">SHOP</a></li>
-                <li><a href="#">REWARDS</a></li>
-            </ul>
+        <div class="footer-layout">
+            <img class="logo-footer" src="CSS/img/logo.png" alt="logo" width="90px" height="90px">
+            <nav class="nav-one">
+                <h1>NAVINGATION</h1>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SHOP</a></li>
+                    <li><a href="#">REWARDS</a></li>
+                </ul> 
+            </nav>
+            <nav class="nav-two">
+                <h1>ACCOUNT</h1>
+                <ul>
+                    <li><a href="#">LOGIN</a></li>
+                    <li><a href="#">REGISTER</a></li>
+                </ul>
+            </nav>
+            <nav class="nav-three">
+                <h1>SOCIAL MEDIA LINKS</h1>
+                <a href="https://instagram.com/hygge_furnitureshop"><img src="CSS/img/insta.png" alt="insta" width="40px" height="40px"></a>
+                <a href="https://twitter.com/hygge_furniture"><img src="CSS/img/twitter.png" alt="twitter" width="40px" height="40px"></a>
+            </nav>
+            <span class="footer_copy">@2022 HYGGE <br> ALL RIGHT RESEVER</span>
         </div>
     </footer>
 </body>
