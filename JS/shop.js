eedadb881fd2f524ce77b8509c003e06c8926067
FONG JIA <fong_jia_yuan@connect.ite.edu.sg>
2022-02-14 15:55:44 +0800

    shop ui is completed

--- JS/shop.js
+++ JS/shop.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.querySelector("#dropbtn");
 const divList = document.querySelector("#myDropdown");
+const triUp = document.querySelector("#triangle-up");
+const triDown = document.querySelector("#triangle-down");
 
 toggleBtn.addEventListener('click', () => {
     if (divList.style.display === 'none') {
@@ -10,10 +12,24 @@ toggleBtn.addEventListener('click', () => {
     }
 });
 
+toggleBtn.addEventListener('click', () => {
+    if (triUp.style.display === 'none') {
+        triUp.style.display = 'block';
+        triDown.style.display = 'none';
+    } 
+    else {
+        triUp.style.display = 'none'
+        triDown.style.display = 'block'
+    }
+});
+
+
 // SECOND DROPDOWN FUNCTION
 
 const toggleBtnOne = document.querySelector("#dropbtn-one");
 const divListOne = document.querySelector("#myDropdown-one");
+const triUpOne = document.querySelector("#triangle-up-one");
+const triDownOne = document.querySelector("#triangle-down-one");
 
 toggleBtnOne.addEventListener('click', () => {
     if (divListOne.style.display === 'none') {
@@ -23,3 +39,78 @@ toggleBtnOne.addEventListener('click', () => {
         divListOne.style.display = 'none'
     }
 });
+
+toggleBtnOne.addEventListener('click', () => {
+    if (triUpOne.style.display === 'none') {
+        triUpOne.style.display = 'block';
+        triDownOne.style.display = 'none';
+    } 
+    else {
+        triUpOne.style.display = 'none'
+        triDownOne.style.display = 'block'
+    }
+});
+
+// THIRD DROPDOWN FUNCTION
+
+const toggleBtnTwo = document.querySelector("#dropbtn-two");
+const divListTwo = document.querySelector("#myDropdown-two");
+const triUpTwo = document.querySelector("#triangle-up-two");
+const triDownTwo = document.querySelector("#triangle-down-two");
+
+toggleBtnTwo.addEventListener('click', () => {
+    if (divListTwo.style.display === 'none') {
+        divListTwo.style.display = 'block';
+    } 
+    else {
+        divListTwo.style.display = 'none'
+    }
+});
+
+toggleBtnTwo.addEventListener('click', () => {
+    if (triUpTwo.style.display === 'none') {
+        triUpTwo.style.display = 'block';
+        triDownTwo.style.display = 'none';
+    } 
+    else {
+        triUpTwo.style.display = 'none'
+        triDownTwo.style.display = 'block'
+    }
+});
+
+// CHECKBOX ITEM
+
+function chairChecked() {
+
+    var checkBox = document.getElementById("chair");
+
+    var chairInfo = document.getElementById("chair-info");
+    var tableInfo = document.getElementById("table-info");
+
+    if (checkBox.checked == true) {
+        chairInfo.style.display = "block";
+        tableInfo.style.display = 'none';
+    } 
+    else  {
+        tableInfo.style.display = 'block';
+    }
+}
+
+function tableChecked() {
+    var checkBoxOne = document.getElementById("table");
+
+    var chairInfoOne = document.getElementById("chair-info");
+    var tableInfoOne = document.getElementById("table-info");
+
+    if (checkBoxOne.checked == true) {
+        tableInfoOne.style.display = 'block';
+        tableInfoOne.style.bottom = "0px";
+        tableInfoOne.style.left = "0px";
+        chairInfoOne.style.display = "none";
+    } 
+    else  {
+        chairInfoOne.style.display = 'block';
+        tableInfoOne.style.bottom = "340px";
+        tableInfoOne.style.left = "320px";
+    }
+}
--- shop.php
+++ shop.php
@@ -15,27 +15,53 @@
       <div class="items-one">
         <h3>FILTER</h3>
         <div class="container-one">
-          <button id="dropbtn" class="dropbtn">Dropdown</button>
+          <div id="triangle-down" class="triangle-down"></div>
+          <div id="triangle-up" class="triangle-up"></div>
+          <button id="dropbtn" class="dropbtn">TYPE</button>
         </div>
         <div id="myDropdown" class="scrollable-checkbox" >
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
+          <input id="sofa" type="checkbox" /> SOFA <br />
+          <input id="dining" type="checkbox" /> DINING TABLE <br />
+          <input id="cardboard" type="checkbox" /> CARDBOARD <br />
+          <input id="bed" type="checkbox" /> BED FRAME <br />
+          <input id="chair" onclick="chairChecked()" type="checkbox" /> CHAIR <br />
+          <input id="table" onclick="tableChecked()" type="checkbox" /> WRITING TABLE <br />
+          <input id="kitchen" type="checkbox" /> KITCHEN <br />
         </div>
         <div class="container-two">
-          <button id="dropbtn-one" class="dropbtn-one">DropdownOne</button>
+          <div id="triangle-down-one" class="triangle-down-one"></div>
+          <div id="triangle-up-one" class="triangle-up-one"></div>
+          <button id="dropbtn-one" class="dropbtn-one">COLOR</button>
         </div>
         <div id="myDropdown-one" class="scrollable-checkbox-one" >
-          <input type="checkbox" /> This is checkbox1 <br />
-          <input type="checkbox" /> This is checkbox <br />
-          <input type="checkbox" /> This is checkbox <br />
+          <input type="checkbox" /> red <br />
+          <input type="checkbox" /> blue <br />
+          <input type="checkbox" /> green <br />
+        </div>
+        <div class="container-three">
+          <div id="triangle-down-two" class="triangle-down-two"></div>
+          <div id="triangle-up-two" class="triangle-up-two"></div>
+          <button id="dropbtn-two" class="dropbtn-one">SIZE</button>
+        </div>
+        <div id="myDropdown-two" class="scrollable-checkbox-two" >
+          <input type="checkbox" /> small <br />
+          <input type="checkbox" /> middem <br />
+          <input type="checkbox" /> large <br />
+          <input type="checkbox" /> cups <br />
         </div>
       </div>
       <div class="items-two">
+        <div class="item-grid">
+          <div id="chair-info" class="chair-info">
+            <img src="CSS/img/chair.png" alt="item-one" width="300px" height="300px">
+            <span> OAK GREEN CHAIR <br> $119.00</span>
+          </div>
+          <div id="table-info" class="table-info">
+            <img src="CSS/img/table.png" alt="item-one" width="300px" height="300px">
+            <span> 105CM WRITING TABLE <br> $599.00</span>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
